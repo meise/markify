520fc51f1ad2c795599658d5fc4d6887945eb688
Daniel Meißner <daniel@3st.be>
2013-04-29 15:10:39 +0200

    Added license to all ruby files.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,22 @@
-# encoding: UTF-8  ,
+# encoding: UTF-8
+=begin
+Copyright Daniel Meißner <meise+markify@3st.be>, 2013
+
+This file is part of Markify.
+
+Markify is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Markify is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Markify. If not, see <http://www.gnu.org/licenses/>.
+=end
 
 require 'simplecov'
 
