f9bff4afd35e97cacee5e8cd635f38e3819c7706
Daniel Meißner <daniel@3st.be>
2013-10-05 17:01:37 +0200

    Added helper function for rspec tests to capture standard output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,3 +37,14 @@ RSpec.configure do |config|
 
   config.order = 'random'
 end
+
+ def capture_stdout(&block)
+  original_stdout = $stdout
+  $stdout = fake = StringIO.new
+  begin
+    yield
+  ensure
+    $stdout = original_stdout
+  end
+  fake.string
+end
